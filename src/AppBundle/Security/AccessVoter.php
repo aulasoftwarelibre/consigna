f3e8fb839014d1c06934d01a280df1ae7c18a5c2
Juan <juanto1990@gmail.com>
2015-04-13 20:03:20 +0200

    voters para controlar la seguridad de acceso y descarga de elementos

---
+++ src/AppBundle/Security/AccessVoter.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AppBundle\Security;
+
+use Symfony\Component\Security\Core\Authorization\Voter\AbstractVoter;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class AccessVoter extends AbstractVoter
+{
+
+    private $sessionInterface;
+
+    const ACCESS = 'access';
+
+
+    protected function getSupportedAttributes()
+    {
+        return array(self::ACCESS);
+    }
+
+    protected function getSupportedClasses()
+    {
+        return array('AppBundle\Entity\File','AppBundle\Entity\Folder');
+    }
+
+    protected function isGranted($attribute, $entity, $user = null)
+    {
+        if (!$user instanceof UserInterface) {
+            if($this->sessionInterface->has($entity->getSlug())){
+                return true;
+            }
+        } else {
+            if($entity->hasAccess($user)){
+                return true;
+            }
+
+            if ($attribute === self::ACCESS && in_array('ROLE_ADMIN', $user->getRoles(), true)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    function __construct(SessionInterface $sessionInterface)
+    {
+        $this->sessionInterface=$sessionInterface;
+    }
+}
---
+++ src/AppBundle/Security/DeleteVoter.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Security;
+
+use Symfony\Component\Security\Core\Authorization\Voter\AbstractVoter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class DeleteVoter extends AbstractVoter
+{
+    const DELETE = 'delete';
+
+    protected function getSupportedAttributes()
+    {
+        return array(self::DELETE);
+    }
+
+    protected function getSupportedClasses()
+    {
+        return array('AppBundle\Entity\File','AppBundle\Entity\Folder');
+    }
+
+    protected function isGranted($attribute, $entity, $user = null)
+    {
+        if ($user instanceof UserInterface) {
+            if ($entity->getUser()===$user){
+                return true;
+            }
+            if ($attribute === self::DELETE && in_array('ROLE_ADMIN', $user->getRoles(), true)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
