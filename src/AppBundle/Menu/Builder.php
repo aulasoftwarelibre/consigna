d23a242ec2103f045c315a77a9c397111529b9dc
Juan <juanto1990@gmail.com>
2015-04-27 17:46:11 +0200

    Adaptada la plantilla base al nuevo estilo

---
+++ src/AppBundle/Menu/Builder.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergio
+ * Date: 26/04/15
+ * Time: 22:27
+ */
+
+namespace AppBundle\Menu;
+
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class Builder extends ContainerAware
+{
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+        $menu->setChildrenAttributes(array('class' => 'sidebar-menu'));
+
+        $menu->addChild('All files', array('route' => 'homepage'));
+
+        return $menu;
+    }
+}
