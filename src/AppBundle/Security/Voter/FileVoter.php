6dbf8d389e860e7739d6c71ea0efc48da49ad2b2
Juan <juanto1990@gmail.com>
2015-04-27 19:05:28 +0200

    cambios en la política de seguridad (voters)

---
+++ src/AppBundle/Security/Voter/FileVoter.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: juanan
+ * Date: 27/04/15
+ * Time: 18:04
+ */
+
+namespace AppBundle\Security\Voter;
+
+
+use AppBundle\Entity\File;
+use AppBundle\Entity\User;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\AbstractVoter;
+
+
+class FileVoter extends AbstractVoter
+{
+    /**
+     * @var
+     */
+    private $session;
+
+    function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
+
+    /**
+     * Return an array of supported classes. This will be called by supportsClass
+     *
+     * @return array an array of supported classes, i.e. array('Acme\DemoBundle\Model\Product')
+     */
+    protected function getSupportedClasses()
+    {
+        return ['AppBundle\Entity\File'];
+    }
+
+    /**
+     * Return an array of supported attributes. This will be called by supportsAttribute
+     *
+     * @return array an array of supported attributes, i.e. array('CREATE', 'READ')
+     */
+    protected function getSupportedAttributes()
+    {
+        return ['ACCESS, DOWNLOAD, DELETE', 'SHARE'];
+    }
+
+    /**
+     * Perform a single access check operation on a given attribute, object and (optionally) user
+     * It is safe to assume that $attribute and $object's class pass supportsAttribute/supportsClass
+     * $user can be one of the following:
+     *   a UserInterface object (fully authenticated user)
+     *   a string               (anonymously authenticated user)
+     *
+     * @param string $attribute
+     * @param File $object
+     * @param User $user
+     *
+     * @return bool
+     */
+    protected function isGranted($attribute, $object, $user = null)
+    {
+        if ($object->getScanStatus() != File::SCAN_STATUS_OK) {
+            return false;
+        }
+        if (in_array('ROLE_ADMIN', $user->getRoles(), true)) {
+            return true;
+        }
+
+        switch ($attribute){
+            case 'ACCESS':
+                if($user || $object->getUser() ){
+                    return true;
+                }
+
+            break;
+
+            case 'DOWNLOAD':
+                if($user){
+                    if($object->hasAccess($user)){
+                        return true;
+                    }
+                } else {
+                    if ($this->session->has($object->getSlug())) {
+                        return true;
+                    }
+                }
+            break;
+
+            case 'DELETE':
+            case 'SHARE':
+                if($object->getUser()==$user){
+                    return true;
+                }
+            break;
+        }
+        return false;
+    }
+}
---
+++ src/AppBundle/Security/Voter/FolderVoter.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: juanan
+ * Date: 27/04/15
+ * Time: 18:09
+ */
+
+namespace AppBundle\Security\Voter;
+
+
+use AppBundle\Entity\Folder;
+use AppBundle\Entity\User;
+use Symfony\Component\Security\Core\Authorization\Voter\AbstractVoter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class FolderVoter extends AbstractVoter
+{
+
+
+    /**
+     * Return an array of supported classes. This will be called by supportsClass
+     *
+     * @return array an array of supported classes, i.e. array('Acme\DemoBundle\Model\Product')
+     */
+    protected function getSupportedClasses()
+    {
+        return ['AppBundle\Entity\Folder'];
+    }
+
+    /**
+     * Return an array of supported attributes. This will be called by supportsAttribute
+     *
+     * @return array an array of supported attributes, i.e. array('CREATE', 'READ')
+     */
+    protected function getSupportedAttributes()
+    {
+        return ['CREATE', 'ACCESS', 'DOWNLOAD', 'DELETE', 'EDIT'];
+    }
+
+    /**
+     * Perform a single access check operation on a given attribute, object and (optionally) user
+     * It is safe to assume that $attribute and $object's class pass supportsAttribute/supportsClass
+     * $user can be one of the following:
+     *   a UserInterface object (fully authenticated user)
+     *   a string               (anonymously authenticated user)
+     *
+     * @param string $attribute
+     * @param Folder $object
+     * @param User $user
+     *
+     * @return bool
+     */
+    protected function isGranted($attribute, $object, $user = null)
+    {
+        if (in_array('ROLE_ADMIN', $user->getRoles(), true)) {
+            return true;
+        }
+
+        switch($attribute) {
+            case 'ACCESS':
+            case 'DOWNLOAD':
+                if($object->hasAccess($user)){
+                    return true;
+                }
+            break;
+
+            case 'DELETE':
+            case 'SHARE' :
+            case 'UPLOAD':
+                if($object->getUser()==$user){
+                    return true;
+                }
+            break;
+        }
+        return false;
+    }
+}
