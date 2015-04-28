309dd60c3bedf8705fd096e8b37247da25996fec
Juan <juanto1990@gmail.com>
2015-04-28 20:11:29 +0200

    Fix: Movido javascript de modales a Resources

--- src/AppBundle/Resources/public/js/consigna.js
+++ src/AppBundle/Resources/public/js/consigna.js
@@ -1,1 +1,22 @@
-$('select').select2();
+$( document ).ready(function() {
+
+    $('select').select2();
+
+    $('#DeleteModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var url = button.data('url') // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        modal.find('#delete').attr('href',url)
+    })
+
+    $('#ShareModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var url = button.data('url') // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        modal.find('#share').attr('value', url)
+    })
+});
