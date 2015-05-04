635b73edba4482c4397a124e812f3fb3b39ab470
Juan <juanto1990@gmail.com>
2015-05-04 18:20:37 +0200

    consigna:organization:enable-> comando para habilitar organization

---
+++ src/AppBundle/Command/EnableOrganizationCommand.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: juanan
+ * Date: 3/05/15
+ * Time: 20:03
+ */
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+class EnableOrganizationCommand extends ContainerAwareCommand{
+
+    protected function configure()
+    {
+        $this
+            ->setName('consigna:organization:enable')
+            ->setDescription('Change organization status to enable')
+            ->addArgument('Code');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $organization = $this->getContainer()->get('doctrine.orm.default_entity_manager')->getRepository('AppBundle:Organization')->findOneByCode($input->getArgument('Code'));
+        if(null===$organization) {
+            $output->writeln('This organization does not exist');
+        } else {
+            $organization->setIsEnabled(true);
+            $output->writeln('Organization status has been updated to enable');
+        }
+    }
+}
