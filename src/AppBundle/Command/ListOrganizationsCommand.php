58d390818653cdccbffaaf5e8f20d71e5ad78215
Juan <juanto1990@gmail.com>
2015-05-04 18:20:16 +0200

    consigna:organization:list->comando para listar las organizaciones.

---
+++ src/AppBundle/Command/ListOrganizationsCommand.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: juanan
+ * Date: 3/05/15
+ * Time: 20:23
+ */
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ListOrganizationsCommand extends ContainerAwareCommand{
+
+    protected function configure()
+    {
+        $this
+            ->setName('consigna:organization:list')
+            ->setDescription('List organizations');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $organizations = $this->getContainer()->get('doctrine.orm.default_entity_manager')->getRepository('AppBundle:Organization')->findAll();
+        foreach ($organizations as $organization){
+            $output->writeln($organization->getCode().' | '.$organization->getName());
+        }
+
+    }
+}
