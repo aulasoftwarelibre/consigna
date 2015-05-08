2537092c483293105364d222b79d0bde99d920b5
Juan <juanto1990@gmail.com>
2015-05-08 18:55:04 +0200

    internacionalización del menú lateral

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -19,10 +19,10 @@ class Builder extends ContainerAware
         $menu = $factory->createItem('root');
         $menu->setChildrenAttributes(array('class' => 'sidebar-menu'));
 
-        $menu->addChild('All files', array('route' => 'homepage'));
+        $menu->addChild($this->container->get('translator')->trans('menu.left.all') , array('route' => 'homepage'));
         if($user = $this->container->get('security.token_storage')->getToken()->getUser()!='anon.'){
-            $menu->addChild('My files', array('route' => 'user_files'));
-            $menu->addChild('Shared with me', array('route' => 'shared_elements'));
+            $menu->addChild($this->container->get('translator')->trans('menu.left.myfiles') , array('route' => 'user_files'));
+            $menu->addChild($this->container->get('translator')->trans('menu.left.share') , array('route' => 'shared_elements'));
         }
 
         return $menu;
