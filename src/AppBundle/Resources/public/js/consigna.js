a0550b462b60930966c44548a3443e40f62d7cd9
Juan <juanto1990@gmail.com>
2015-05-24 20:17:02 +0200

    ahora se puede copiar la dirección de compartición con clipboard

--- src/AppBundle/Resources/public/js/consigna.js
+++ src/AppBundle/Resources/public/js/consigna.js
@@ -16,7 +16,10 @@ $( document ).ready(function() {
         var url = button.data('url') // Extract info from data-* attributes
         // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
         // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-        var modal = $(this)
-        modal.find('#share').attr('value', url)
+        var modal = $(this);
+        modal.find('#share').attr('value', url).select();
+        modal.find('#sharebtn').attr('data-clipboard-text', url);
     })
+
+    var client = new ZeroClipboard( $("button#sharebtn") );
 });
