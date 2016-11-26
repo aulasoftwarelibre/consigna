96efbc5a018b0b2f0c00edc98b8ff6ca4f0b0688
Sergio Gomez <sergio@uco.es>
2016-11-26 16:42:07 +0100

    Updated Commands

--- src/AppBundle/Command/ScanFilesCommand.php
+++ src/AppBundle/Command/ScanFilesCommand.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is part of the Consigna project.
+ *
+ * (c) Juan Antonio Martínez <juanto1990@gmail.com>
+ * (c) Sergio Gómez <sergio@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace AppBundle\Command;
 
@@ -6,6 +15,7 @@ use AppBundle\Entity\File;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Exception\FileNotFoundException;
 
 class ScanFilesCommand extends ContainerAwareCommand
 {
@@ -31,12 +41,13 @@ class ScanFilesCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $files = $this->getContainer()->get('consigna.repository.file')->findAll();
+        $adapter = new ClamAvAdapter($this->getClamavPath());
 
         $em = $this->getContainer()->get('doctrine.orm.entity_manager');
         /** @var File $file */
         foreach ($files as $file) {
-            $result = $this->getContainer()->get('consigna.service.scan_file')->scan($file->getPath());
-            if ($result['status'] !== 'OK') {
+            $result = $adapter->scan([$file->getPath()]);
+            if ($result->hasVirus()) {
                 $output->writeln($this->getContainer()->get('translator')->trans('action.scan_virus_detected', ['%file%' => $file->getPath()], 'command'));
                 $file->setScanStatus(File::SCAN_STATUS_INFECTED);
             } else {
@@ -48,5 +59,24 @@ class ScanFilesCommand extends ContainerAwareCommand
         $em->flush();
 
         $output->writeln($this->getContainer()->get('translator')->trans('action.scan_files_success', [], 'command'));
+    }
+
+    private function getClamavPath()
+    {
+        $default_paths = [
+            '/usr/bin/clamdscan',
+            '/usr/local/bin/clamdscan',
+            '/opt/bin/clamdscan',
+        ];
+
+        array_unshift($default_paths, $this->getContainer()->getParameter('antivirus_path'));
+
+        foreach ($default_paths as $path) {
+            if ($path && file_exists($path)) {
+                return $path;
+            }
+        }
+
+        throw new FileNotFoundException($this->getContainer()->get('translator')->trans('action.scan_files_missing', [], 'command'));
     }
 }
--- src/AppBundle/Command/ScrappingOrganizationsCommand.php
+++ src/AppBundle/Command/ScrappingOrganizationsCommand.php
@@ -1,10 +1,12 @@
 <?php
-
 /**
- * Created by PhpStorm.
- * User: sergio
- * Date: 02/05/15
- * Time: 05:34.
+ * This file is part of the Consigna project.
+ *
+ * (c) Juan Antonio Martínez <juanto1990@gmail.com>
+ * (c) Sergio Gómez <sergio@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace AppBundle\Command;
