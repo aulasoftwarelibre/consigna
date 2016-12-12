4ca378dcd1854889eb21eac9f4516c09cd5e4003
Sergio Gómez <sergio@uco.es>
2016-12-12 21:53:43 +0100

    Moved fixtures

--- src/AppBundle/Doctrine/DataFixtures/Alice/Provider/ConsignaProvider.php
+++ src/AppBundle/Doctrine/DataFixtures/Alice/Provider/ConsignaProvider.php
@@ -1,13 +1,15 @@
 <?php
-
 /**
- * Created by PhpStorm.
- * User: sergio
- * Date: 15/4/15
- * Time: 9:05.
+ * This file is part of the Consigna project.
+ *
+ * (c) Juan Antonio Martínez <juanto1990@gmail.com>
+ * (c) Sergio Gómez <sergio@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
-namespace MovedBundle\DataFixtures\Alice\Provider;
+namespace AppBundle\Doctrine\DataFixtures\Alice\Provider;
 
 class ConsignaProvider
 {
